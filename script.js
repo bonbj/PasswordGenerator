a3c83ed8a760e19a136f4d89a97f9f990cd94a9d
joao victor <jaotipereira@gmail.com>
2020-01-25 22:10:44 -0200

    Adding filter in the algorithm

--- script.js
+++ script.js
@@ -2,25 +2,53 @@
 const password = document.getElementById('password');
 
 //random integer
-function getRandomNum(lbound, ubound) {
+getRandomNum = (lbound, ubound) => {
   return Math.floor(Math.random() * (ubound - lbound)) + lbound;
 }
 
 //random character
-function getRandomChar() {
-  var numberChars = "0123456789";
-  var lowerChars = "abcdefghijklmnopqrstuvwxyz";
-  var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var otherChars = "`~!@#$%^&*()-_=+[{]}\\|;:'\",./?";
-  var charSet = numberChars + lowerChars + upperChars + otherChars;
+getRandomChar = (number,lower,upper,other) => {
+
+  //options
+  const numberChars = "0123456789";
+  const lowerChars = "abcdefghijklmnopqrstuvwxyz";
+  const upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const otherChars = "`~!@#$%^&*()-_=+[{]}\\|;:'\",./?";
+
+  let charSet = "";
+
+  //filter
+  if(number === true){
+    charSet = charSet + numberChars;
+  }
+
+  if(lower === true){
+    charSet = charSet + lowerChars;
+  }
+
+  if(upper === true){
+    charSet = charSet + upperChars;
+  }
+
+  if(other === true){
+    charSet = charSet + otherChars;
+  }
+  
   return charSet.charAt(getRandomNum(0, charSet.length));
 }
 
 //password
-let srt = "";
-for(let i=0;i<20;i++){
-  srt=srt+ getRandomChar();
+key = (measure,number,lower,upper,other) => {
+
+  //generate password by length
+  let srt = "";
+  for(let i=0;i<measure;i++){
+    srt=srt+ getRandomChar(number,lower,upper,other);
+  }
+
+  //show in html
+  password.innerHTML = srt;
 }
 
-//show in html
-password.innerHTML = srt;
+//call the password
+key(20,true,true,true,true);
